a02573a89613fbd755584710689417ea03e40280
Andrey Ivanov <veny.xd@gmail.com>
2022-08-24 14:42:43 +0300

    empty config

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_API_TOKEN = ''
+OPENWEATHER_API_KEY = ''
+
+OPENWEATHER_API_URL = (
+        'https://api.openweathermap.org/data/2.5/weather?'
+        'lat={latitude}&lon={longitude}&'
+        'appid=' + OPENWEATHER_API_KEY + '&units=metric'
+)
